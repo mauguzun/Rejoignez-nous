cc42f935ecc0f1d351d1aaefdeb37888f28061e3
mauguzun <mauguzun@gmail.com>
2020-01-02 13:19:58 +0200

    2020 statrt

--- application/config/config.php
+++ application/config/config.php
@@ -25,7 +25,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 /*$config['base_url'] = 'http://asl.nl.eu.org/';
 */
-$config['base_url'] = 'http://localhost/reg/';
+$config['base_url'] = 'http://localhost/Rejoignez-nous/';
 /*$config['base_url'] = 'https://careers.aslairlines.com/';
 */
 /*
