724998e18980320b9cf3c319bc44c2dcea37c683
illarionmedv <lars200020@gmail.com>
2020-07-07 23:22:04 +0300

    add example job description

--- application/views/front/job_description.php
+++ application/views/front/job_description.php
@@ -1,79 +1,100 @@
-<div class="career_galley_banner" style="background-image: url('<?= base_url() ?>static/update/img/Frise-picto-RH---1378-x-317px.jpg');">
-<div class="equal_opp_title">PILOT</div>
+<div class="job_description_banner" style="background-image: url('<?= base_url() ?>static/update/img/100310_F-GZTC_en_vol_P._Sordoillet_18.png');">
+<div class="job_description_banner_title">PILOT</div>
 </div>
 
-<div class="container" style="margin-bottom: 80px;">
-<div class="row">
-    
-    <div class="col-md-3 job_gallery_white">
-    <a href="<?= base_url() ?>job_description">
-        <div class="job_gallery_inner_container">
-            <div class="job_gallery_text">PILOT</div>
-            <center><img src="<?= base_url() ?>static/update/img/pilot.png" alt="" class="job_gallery_img"></center>
+<div class="custom_body_heading">BE A PILOT AT ASL AIRLINES</div>
+
+<div class="container">
+
+
+    <div class="row" style="margin-top: 50px;">
+        <div class="col-md-4">
+            <img src="<?= base_url() ?>static/update/img/_PIC7072.png" alt="" style="width: 250px;">
+        </div>
+        <div class="col-md-8">
+            <div class="custom_dropdown_feedback" style="padding-left: 50px; font-style: inherit!important; color: black !important;">
+                        <img src="<?= base_url() ?>static/update/img/Format_Quote_Icon_2.png" alt="" class="upper_bracket">
+                        My job is to get passengers from point A to point B safely. I joined ASL as an Airline Transport Pilot Officer and quickly progressed to captain. Today, I'm an instructor and chief pilot.  
+                                <img src="<?= base_url() ?>static/update/img/Format_Quote_Icon_3 (1).png" alt="" class="lower_bracket">
+            </div>
+
+            <div class="custom_dropdown_feedback_author">Frederic
+                <br>
+                Chief Pilot at ASL AIRLINES
+                </div>
+        </div>
+    </div>
+
+<div class="in_breif_title">In brief ...</div>
+
+    <div class="row" style="margin-bottom: 100px;">
+        <div class="col-md-4">
+            <center>
+                <img src="<?= base_url() ?>static/update/img/birthday_1.png" alt="" class="in_brief_img">
+                <div class="in_brief_bold">
+                    In 2000
+                </div>
+                <div class="in_brief_small">Entry at asl airlines</div>
+            </center>
+        </div>
+        <div class="col-md-4">
+        <center>
+                <img src="<?= base_url() ?>static/update/img/around_1.png" alt="" class="in_brief_img">
+                <div class="in_brief_bold">
+                    2 240 254
+                </div>
+                <div class="in_brief_small">Km travelled, the equivalent of 56 round of the earth</div>
+            </center>
+        </div>
+        <div class="col-md-4">
+        <center>
+                <img src="<?= base_url() ?>static/update/img/clock_1.png" alt="" class="in_brief_img">
+                <div class="in_brief_bold">
+                    12 750 
+                </div>
+                <div class="in_brief_small">Flight hours in 20 years</div>
+            </center>
+        </div>
+    </div>
+
+    <center><a href="<?= base_url() ?>offers" class="check_latest_offers">Check our latest offers</a></center>
+    <div class="custom_hr"></div>
+
+
+    <div class="employment_link_tab">
+        <div class="employment_link_title">EMPLOYMENT</div>
+
+        <a href="<?= base_url() ?>job_description" class="employment_body_link"><div class="employment_body_link_block">
+            Planning agent
+            <img src="<?= base_url() ?>static/update/img/Keyboard_Arrow_Right_Icon_1.png" alt="">   
+        </div></a>
+        <a href="<?= base_url() ?>job_description" class="employment_body_link"><div class="employment_body_link_block">
+            Steward / Flight attendant
+
+            <img src="<?= base_url() ?>static/update/img/Keyboard_Arrow_Right_Icon_1.png" alt="">   
+        </div></a>
+        <a href="<?= base_url() ?>job_description" class="employment_body_link"><div class="employment_body_link_block">
+        Operating officer
+            <img src="<?= base_url() ?>static/update/img/Keyboard_Arrow_Right_Icon_1.png" alt="">   
+        </div></a>
+        <a href="<?= base_url() ?>job_description" class="employment_body_link"><div class="employment_body_link_block">
+        Commercial
+            <img src="<?= base_url() ?>static/update/img/Keyboard_Arrow_Right_Icon_1.png" alt="">   
+        </div></a>
+        <a href="<?= base_url() ?>job_description" class="employment_body_link"><div class="employment_body_link_block">
+        Operation manager
+            <img src="<?= base_url() ?>static/update/img/Keyboard_Arrow_Right_Icon_1.png" alt="">   
+        </div></a>
+        <a href="<?= base_url() ?>job_description" class="employment_body_link"><div class="employment_body_link_block">
+        Ground agent
+            <img src="<?= base_url() ?>static/update/img/Keyboard_Arrow_Right_Icon_1.png" alt="">   
+        </div></a>
+        <a href="<?= base_url() ?>job_description" class="employment_body_link"><div class="employment_body_link_block" style="border:0px !important;">
+        Aircraft Mechanic
+            <img src="<?= base_url() ?>static/update/img/Keyboard_Arrow_Right_Icon_1.png" alt="">   
         </div></a>
     </div>
-    
-    <div class="col-md-3 job_gallery_blue">
-    <a href="<?= base_url() ?>job_description">
-    <div class="job_gallery_inner_container">
-        <div class="job_gallery_text">FLIGHT ATTENDANT / 
-            <br>STEWARD
-        </div>
-        <center><img src="<?= base_url() ?>static/update/img/people.png" alt="" class="job_gallery_img"></center>
-    </div>
-    </a>
-    </div>
-    <div class="col-md-3 job_gallery_white">
-    <a href="<?= base_url() ?>job_description">
-    <div class="job_gallery_inner_container">
-        <div class="job_gallery_text">PLANNING OFFICER </div>
-        <center><img src="<?= base_url() ?>static/update/img/checklist.png" alt="" class="job_gallery_img"></center>
-    </div>
-    </a>
-    </div>
-    <div class="col-md-3 job_gallery_blue">
-    <a href="<?= base_url() ?>job_description">
-    <div class="job_gallery_inner_container">
-        <div class="job_gallery_text">OPERATING OFFICER</div>
-        <center><img src="<?= base_url() ?>static/update/img/engineer.png" alt="" class="job_gallery_img"></center>
-    </div>
-    </a>
-    </div>
-</div>
-<div class="row">
-    <div class="col-md-3 job_gallery_blue">
-    <a href="<?= base_url() ?>job_description">
-        <div class="job_gallery_inner_container">
-            <div class="job_gallery_text">COMMERCIAL</div>
-            <center><img src="<?= base_url() ?>static/update/img/market.png" alt="" class="job_gallery_img"></center>
-        </div>
-        </a>
-    </div>
-    <div class="col-md-3 job_gallery_white">
-    <a href="<?= base_url() ?>job_description">
-    <div class="job_gallery_inner_container">
-        <div class="job_gallery_text">OPERATION MANAGER
-        </div>
-        <center><img src="<?= base_url() ?>static/update/img/agent_d'exploitation.png" alt="" class="job_gallery_img"></center>
-    </div>
-    </a>
-    </div>
-    <div class="col-md-3 job_gallery_blue">
-    <a href="<?= base_url() ?>job_description">
-    <div class="job_gallery_inner_container">
-        <div class="job_gallery_text">GROUND AGENT</div>
-        <center><img src="<?= base_url() ?>static/update/img/runway.png" alt="" class="job_gallery_img"></center>
-    </div>
-    </a>
-    </div>
-    <div class="col-md-3 job_gallery_white">
-    <a href="<?= base_url() ?>job_description">
-    <div class="job_gallery_inner_container">
-        <div class="job_gallery_text">AIRCRAFT MECHANIC</div>
-        <center><img src="<?= base_url() ?>static/update/img/mechanic.png" alt="" class="job_gallery_img"></center>
-    </div>
-    </a>
-    </div>
+
 </div>
 
 
